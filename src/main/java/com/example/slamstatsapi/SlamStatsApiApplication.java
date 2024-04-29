c57e9493c8da4bc03dc0a9ea27e0990e03e806bf
Mariskii <vicentpayaginer@gmail.com>
2024-04-29 20:11:03 +0200

    Fix problems at the cors

--- src/main/java/com/example/slamstatsapi/SlamStatsApiApplication.java
+++ src/main/java/com/example/slamstatsapi/SlamStatsApiApplication.java
@@ -13,16 +13,4 @@ public class SlamStatsApiApplication {
 		SpringApplication.run(SlamStatsApiApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
 }
